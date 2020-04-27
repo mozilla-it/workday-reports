2086d505aa68bbad905d38e25eed1e85f35594b2
Chris Valaas <cvalaas@mozilla.com>
2020-04-27 15:28:46 -0700

    slight change

--- WorkdayReports/raw_reports.py
+++ WorkdayReports/raw_reports.py
@@ -5,23 +5,36 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class RawReports():
-
-    def __init__(self, report_url: str, username: str = None, password: str = None, proxies: dict = {}):
+class RawReports:
+    def __init__(
+        self,
+        report_url: str = None,
+        username: str = None,
+        password: str = None,
+        proxies: dict = {},
+    ):
         self.report_url = report_url
         self.username = username
         self.password = password
         self.proxies = proxies
 
-    def get_report(self) -> str:
+    def get_report(
+        self, report_url: str = None, username: str = None, password: str = None
+    ) -> str:
+
+        report_url = report_url if report_url else self.report_url
+        username = username if username else self.username
+        password = password if password else self.password
+
         try:
-            if self.username and self.password:
-                r = requests.get(self.report_url, auth=(self.username, self.password), proxies=self.proxies)
+            if username and password:
+                r = requests.get(
+                    report_url, auth=(username, password), proxies=self.proxies,
+                )
             else:
-                r = requests.get(self.report_url, proxies=self.proxies)
+                r = requests.get(report_url, proxies=self.proxies)
             r.encoding = "utf-8"  # otherwise requests thinks it's ISO-8859-1
             return r.text
         except:
             logger.critical(sys.exc_info()[0])
             raise
-
