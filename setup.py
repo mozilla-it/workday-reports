b1613aa46df53636a62f84c09efe916aa24324ee
Chris Valaas <cvalaas@mozilla.com>
2020-04-29 13:46:02 -0700

    a couple changes for builds

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ requirements = [
 
 setup(
     name="workday-reports",
-    version="0.0.1",
+    version="0.0.2",
     description="Workday reports-getter",
     python_requires=">=3.4",
     author="IT Data Team",
     author_email="data-services@mozilla.com",
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests", "*no_dist*"]
+    ),
     # scripts=[s for s in setuptools.findall("bin/") if os.path.splitext(s)[1] != ".pyc"],
     install_requires=requirements,
 )
